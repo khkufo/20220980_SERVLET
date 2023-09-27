b13c6ff4452e6d7e6526b2bca2e920e51792b6a9
khkufo <khkufo@naver.com>
2023-09-27 05:04:02 +0000

    5

--- WEB-INF/src/dao/ProductRepository.java
+++ WEB-INF/src/dao/ProductRepository.java
@@ -1,6 +1,8 @@
 package dao;
 import java.util.ArrayList;
 import dto.Product;
+<jsp:useBean id="productDAO" class="dao.ProductRepository" scope="session" />
+    // <%ProductRepository test=new ProductRepository();%> 와 동일
 
 public class ProductRepository{
     private ArrayList<Product> listOfProducts = new ArrayList<Product>(); //ArrayList를 Product로 캐스팅
@@ -10,6 +12,30 @@ public class ProductRepository{
         phone.setCategory("Smart Phone");
         phone.setManufacturer("Apple");
         phone.setUnitsInStock(1000);
-        phone.setConditionon("New");
+        phone.setCondition("New");
+        
+        Product notebook = new Product ("P1235","LG PC 그램",1500000);
+        notebook.setDescription("13.3-inch,IPS LED display, 5rd Generation Intel Core processors")
+        notebook.setCategory("Notebook");
+        notebook.setManufacturer("LG");
+        notebook.setUnitsInStock(1000);
+        notebook.setCondition("Refurbished");
+        
+        Product tablet = new Product("P1236","Galaxy Tab S", 900000);
+        tablet.setDescription("212.8*125.6*6mm, Super AMOLED display, Octa-Core processor");
+        tablet.setCategory("Tablet");
+        tablet.setManufacturer("Samsung");
+        tablet.setUnitsInStock(1000);
+        tablet.setCondition("Old");
+        
+        //위와 같이 상품 초기화 하고 아래에 상품을 추가
+        
+        listOfProducts.add(phone);
+        listOfProducts.add(notebook);
+        listOfProducts.add(tablet);
+            //listOfProducts.add(상품명);
+    }
+    public ArrayList<Product> getAllProducts(){
+        return listOfProducts;
     }
 }

460b7f996e4c7096101ba0363c88590b626a7e5e
khkufo <khkufo@naver.com>
2023-10-11 05:26:05 +0000

    7

--- WEB-INF/src/dao/ProductRepository.java
+++ WEB-INF/src/dao/ProductRepository.java
@@ -5,6 +5,11 @@ import dto.Product;
 public class ProductRepository {
 
 	private ArrayList<Product> listOfProducts = new ArrayList<Product>();
+    private static ProductRepository instance = new ProductRepository();
+    
+    public static ProductRepository getInstance(){
+        return instance;
+    }
 
 	public ProductRepository() {
 		Product phone = new Product("P1234", "iPhone 6s", 800000);
@@ -52,4 +57,8 @@ public class ProductRepository {
         }
         return productById;
     }
+}
+
+public void addProduct(Product product){
+    listOfProducts.add(product);
 }
